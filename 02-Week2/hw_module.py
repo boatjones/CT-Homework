b1a1ac8036b1b6d67709a397027206930b48f335
Randall Thomas <trt227@stern.nyu.edu>
2023-09-20 23:03:27 -0400

    Week2-Day3 Python modules and dictionaries
    In addition to notebook for assignment, module pushed up and thus the dunder pycache directory for module

---
+++ 02-Week2/hw_module.py
@@ -0,0 +1,11 @@
+# external code imported in cell below
+import math
+
+def bldg_sq_ft(lgth, wdth, floors):
+    return lgth * wdth * floors
+
+def circumf(radius):
+    return radius * math.pi
+
+def inches(feet):
+    return feet * 12
